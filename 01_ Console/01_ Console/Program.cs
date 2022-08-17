cb05233ba9741f9f0aab543addc27b71b53d20ff
skl509 <77062634+skl509@users.noreply.github.com>
2022-08-17 12:17:30 +0900

    Update Program.cs

--- 01_ Console/01_ Console/Program.cs
+++ 01_ Console/01_ Console/Program.cs
@@ -49,50 +49,65 @@ namespace _01__Console
             bool b2 = false;
 
 
-            int level = 1;
-            int HP = 10;
-            float exp = 0.9f;
+            int level = 3;
+            int HP = 11;
+            float exp = 0.1f;
             string name = "너굴맨";
-            string result = $"{name}의 레벨은 {level}이고 HP는 {HP}이고 exp는 {exp}다. \n";
+            //string result = $"{name}의 레벨은 {level}이고 HP는 {HP}이고 exp는 {exp}다. \n";
 
-            //너굴맨의 레벨은 1이고 HP는 10이고 exp는 0.9다.
+            ////너굴맨의 레벨은 1이고 HP는 10이고 exp는 0.9다.
 
-            Console.WriteLine(result);
-            //강사님이 한것 1
-            Console.WriteLine($"이름 : {name}\n레벨 : {level}\nHP : {HP}\nexp : {exp}");
-            //강사님이 한것 2
-            Console.WriteLine(name + "의 레벨은" + level + " 이고 HP는 " + HP + "이고 exp는" + exp + " 이다");
-            //내가한것
+            //Console.WriteLine(result);
+            ////강사님이 한것 1
+            //Console.WriteLine($"이름 : {name}\n레벨 : {level}\nHP : {HP}\nexp : {exp}");
+            ////강사님이 한것 2
+            //Console.WriteLine(name + "의 레벨은" + level + " 이고 HP는 " + HP + "이고 exp는" + exp + " 이다");
+            ////내가한것
 
-            Console.Write("이름을 입력하세요 : ");
-            name = Console.ReadLine();
-            Console.Write($"{name}의 레벨을 입력하세요 : ");
-            string temp = Console.ReadLine();
-            // Console 같은 함수기능은 string 만 받아서 int형 이용 할려면 변형해야된다.
-            //level = int.Parse(temp);// string을 int로 변경해주는 함수(숫자로 바꿀 수 있을 때만 가능)
-            //int.TryParse{temp, out level}; // string을 int로 변경해주는 코드 (숫자를 바꿀 수없으면 0으로 만든다)
-            // 안전하게 이용 가능한 함수 -> 이걸로 이용 추천!
-            //level = Convert.ToInt32(temp); // string을 int로 변경해주는 코드(숫자로 바꿀 수 있을때만 가능)
-            int.TryParse(temp, out level);
-            Console.Write($"{name}를 HP를 입력하세요 : ");
-            string temp2 = Console.ReadLine();
-            int.TryParse(temp2, out HP);
-            Console.Write($"{name}를 exp를 입력하세요 : ");
-            string temp3 = Console.ReadLine();
-            float.TryParse(temp3, out exp);
-            
-            result = $"{name}의 레벨은 {level}이고 HP는 {HP}이고 exp는 {exp}다. \n";
-            Console.WriteLine(result);
+            //Console.Write("이름을 입력하세요 : ");
+            //name = Console.ReadLine();
+            //Console.Write($"{name}의 레벨을 입력하세요 : ");
+            //string temp = Console.ReadLine();
+            //// Console 같은 함수기능은 string 만 받아서 int형 이용 할려면 변형해야된다.
+            ////level = int.Parse(temp);// string을 int로 변경해주는 함수(숫자로 바꿀 수 있을 때만 가능)
+            ////int.TryParse{temp, out level}; // string을 int로 변경해주는 코드 (숫자를 바꿀 수없으면 0으로 만든다)
+            //// 안전하게 이용 가능한 함수 -> 이걸로 이용 추천!
+            ////level = Convert.ToInt32(temp); // string을 int로 변경해주는 코드(숫자로 바꿀 수 있을때만 가능)
+            //int.TryParse(temp, out level);
+            //Console.Write($"{name}를 HP를 입력하세요 : ");
+            //string temp2 = Console.ReadLine();
+            //int.TryParse(temp2, out HP);
+            //Console.Write($"{name}를 exp를 입력하세요 : ");
+            //string temp3 = Console.ReadLine();
+            //float.TryParse(temp3, out exp);
 
-            //이름, 레벨, hp, 경험치를 각각 입력 받고 출력하는 코드 만들기
+            //result = $"{name}의 레벨은 {level}이고 HP는 {HP}이고 exp는 {exp * 100:F3}%다. \n";
+            //Console.WriteLine(result);
 
-            exp = 0.959595f;
+            ////이름, 레벨, hp, 경험치를 각각 입력 받고 출력하는 코드 만들기
+
+            //exp = 0.959595f;
             //result = $"{name}의 레벨은 {level}이고 HP는 {HP}이고 exp는 {exp*100:F3}다. \n"; //:F3는 소수좀 3자리까지만 춢력하라는 것
             //Console.WriteLine(result);
 
+            //변수 종료----------------------------------------------------------------------------------------------------
 
+            // 제어문(Control state)
+            // 실행되는 코드 라인을 변경할 수 있는 코드
+
+            if (HP < 3) // HP가 2이기 때문에 참이다
+            {
+                Console.WriteLine("HP가 부족합니다"); // 참이라서 아래코드가 실행! 
+            }
+            else if (HP < 10)
+            {
+                Console.WriteLine("HP가 적당합니다");
+            }
+            else
+            {
+                Console.WriteLine("HP가 충분합니다");
+            }
             
-
 
             Console.ReadKey(); // 키 입력 대기코드
         }
