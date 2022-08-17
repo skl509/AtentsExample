2f959da842d1bb376a5624d3bbbfce8939045d02
skl509 <77062634+skl509@users.noreply.github.com>
2022-08-17 12:53:08 +0900

    Update Program.cs

--- 01_ Console/01_ Console/Program.cs
+++ 01_ Console/01_ Console/Program.cs
@@ -94,7 +94,7 @@ namespace _01__Console
 
             // 제어문(Control state)
             // 실행되는 코드 라인을 변경할 수 있는 코드
-
+            HP = 10;
             if (HP < 3) // HP가 2이기 때문에 참이다
             {
                 Console.WriteLine("HP가 부족합니다"); // 참이라서 아래코드가 실행! 
@@ -107,7 +107,43 @@ namespace _01__Console
             {
                 Console.WriteLine("HP가 충분합니다");
             }
-            
+
+            switch (HP)
+            {
+                case 0: //HP가 0일때
+                    Console.WriteLine("HP가 0입니다");
+                    break;
+                case 5:  //HP가 5일때
+                    Console.WriteLine("HP가 5입니다");
+                    break;
+                default:  // 위에 설정되지 않은 모든 경우
+                    Console.WriteLine("HP가 0과 5가 아닙니다.");
+                    break;
+            }
+
+
+
+            Console.WriteLine("현재 경험치는" + exp + " 입니다!\n");
+            Console.WriteLine("경험치를 추가합니다.");
+            Console.Write("추가할 경험치 : "); // 이전의 exp 값이 안 덮여 쓰도록 이전 값의 저장할 수 있도록
+                                         // 변수를 새로 만들어준다
+            float floatTemp = 0.0f; // 새로 입력 받는 경험치 저장하는 변수
+            string temp = Console.ReadLine(); // 추가 경험치 입력받는 것1
+            float.TryParse(temp, out floatTemp);// 추가 경험치 입력받는 것2
+            exp = exp + floatTemp; // 이전에 있는 exp 값 에다가 새로 입력하는 floatTemp 를 더해주기!
+            if (exp >= 1)
+            {
+                Console.WriteLine("레벨 업!");
+            }
+            else
+            {
+                Console.WriteLine("현제 경험치는" + exp + " 입니다!");
+
+            }
+
+            //실습 : exp의 값과 추가로 입력받은 경험치의 합이 1 이상이면 레벨 업 이라고 출력하고
+            //       1 미만이면 현재 경험치 합계를 출력하는 코드 작성하기
+
 
             Console.ReadKey(); // 키 입력 대기코드
         }
