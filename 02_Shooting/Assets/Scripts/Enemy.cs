194280858cf6a94d4ba1ec7b721ae7aa6c27f70f
skl509 <77062634+skl509@users.noreply.github.com>
2022-08-29 14:04:23 +0900

    월드, 로컬 좌표

---
+++ 02_Shooting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Vector3.left, Space.Self);
+    }
+
+}
